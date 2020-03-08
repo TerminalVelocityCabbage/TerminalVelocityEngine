9654ddf0e2b45f14ad3ec866ad65f93042e95f14
Brandon Davis <neusfear@gmail.com>
2020-03-08 16:05:30 -0500

    Abstract Server to engine so that the impl can be unique

---
+++ src/main/java/engine/server/ServerBase.java
@@ -0,0 +1,72 @@
+package engine.server;
+
+import com.github.simplenet.Server;
+import engine.configs.Config;
+import engine.debug.Log;
+import engine.saves.SaveManager;
+import org.fusesource.jansi.AnsiConsole;
+
+import java.io.File;
+import java.io.IOException;
+
+public abstract class ServerBase {
+
+	String id;
+	Server server;
+
+	public ServerBase(String id) {
+		this.id = id;
+	}
+
+	public void init() {
+		preInit();
+		//Enable Console colors
+		AnsiConsole.systemInstall();
+
+		//Start up the network listeners
+		server = new Server();
+	}
+
+	//Basically set the server files up before starting any connections
+	private void preInit() {
+		//Check for and load or create new server configuration
+		Log.info("Server Starting with ID: " + id);
+		if (!SaveManager.checkForSaveDirectory(id)) {
+			Log.info("No save Directory found for this save, assuming its a new save.");
+			if (SaveManager.createSaveDirectory(id)) {
+				Config config = Config.builder()
+						.setFileName("server")
+						.setPath("saves" + File.separator + id)
+						.addKey("ip", "localhost")
+						.addKey("port", "49056")
+						.addKey("motd", "Message of the Day!")
+						.build();
+				if (config.save()) {
+					return;
+				}
+			}
+		} else {
+			Log.info("Found existing save with name " + id + " loading that save.");
+			return;
+		}
+		Log.error("Something went wrong during server initialization, the server will not start.");
+	}
+
+	//Binds the server and begins allowing connections
+	public void start() {
+		//Load the server's config file into a usable object
+		try {
+			Config config = Config.load("saves" + File.separator + this.id , "server");
+			//Bind the server to the configured port and IP
+			server.bind(config.getOptions().get("ip"),
+					Integer.parseInt(config.getOptions().get("port")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Server getServer() {
+		return this.server;
+	}
+
+}
--- src/main/java/server/GameServer.java
+++ src/main/java/server/GameServer.java
@@ -1,60 +1,25 @@
 package server;
 
 import com.github.simplenet.Client;
-import com.github.simplenet.Server;
 import com.github.simplenet.packet.Packet;
-import engine.configs.Config;
 import engine.debug.Log;
-import engine.saves.SaveManager;
-import org.fusesource.jansi.AnsiConsole;
+import engine.server.ServerBase;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class GameServer {
-
-	String id = "default";
+public class GameServer extends ServerBase {
 
 	public GameServer(String id) {
-		this.id = id;
+		super(id);
 	}
 
+	@Override
 	public void init() {
-		Log.info("Server Starting with ID: " + id);
-		if (!SaveManager.checkForSaveDirectory(id)) {
-			Log.info("No save Directory found for this save, assuming its a new save.");
-			if (SaveManager.createSaveDirectory(id)) {
-				Config config = Config.builder()
-						.setFileName("config")
-						.setPath("saves" + File.separator + id)
-						.addKey("ip", "localhost")
-						.addKey("port", "49056")
-						.addKey("motd", "Message of the Day!")
-						.build();
-				if (config.save()) {
-					start();
-					return;
-				}
-			}
-		} else {
-			Log.info("Found existing save with name " + id + " loading that save.");
-			start();
-		}
-		Log.error("Something went wrong during server initialization, the server will not start.");
-	}
-
-	public void start() {
-		//Enable Console colors
-		AnsiConsole.systemInstall();
-
-		//Start up the network listeners
-		var server = new Server();
-
+		super.init();
 		//Create a map of the clients and their usernames
 		var clients = new ConcurrentHashMap<Client, String>();
 
-		server.onConnect(client -> {
+		getServer().onConnect(client -> {
 
 			Log.info("Client attempting to establish connection.");
 
@@ -76,23 +41,17 @@ public class GameServer {
 					case 2: //Send message to connected clients
 						client.readString(message -> {
 							message = clients.get(client) + ": " + message;
-							server.queueAndFlushToAllExcept(Packet.builder().putString(message), client);
+							getServer().queueAndFlushToAllExcept(Packet.builder().putString(message), client);
 						});
 						break;
 				}
 			});
 		});
-
-		//Load the server's config file into a usable object
-		try {
-			Config config = Config.load("saves" + File.separator + this.id , "config");
-			//Bind the server to the configured port and IP
-			//TODO read from server config
-			server.bind(config.getOptions().get("ip"),
-					Integer.parseInt(config.getOptions().get("port")));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		this.start();
 	}
 
+	@Override
+	public void start() {
+		super.start();
+	}
 }
