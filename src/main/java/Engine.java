4fe356e5b293bdfb4ee779ef3298bfc4b9fe73ca
Brandon Davis <neusfear@gmail.com>
2020-03-18 13:06:19 -0500

    remove client server and game from engine repo

---
+++ src/main/java/Engine.java
@@ -0,0 +1,7 @@
+public class Engine {
+
+	public static void main(String[] args) {
+		System.out.println("The engine itself should not be run directly. See repository for info on how to use TVE.");
+	}
+
+}
