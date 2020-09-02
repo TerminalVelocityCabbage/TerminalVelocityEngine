6214e32fd358710ef6a4d035da7a0bacd33a7700
Brandon Davis <neusfear@gmail.com>
2020-09-02 10:31:27 -0500

    remove duplicate files

--- src/main/java/Engine.java
+++
@@ -1,7 +0,0 @@
-public class Engine {
-
-	public static void main(String[] args) {
-		System.out.println("The engine itself should not be run directly. See repository for info on how to use TVE.");
-	}
-
-}
