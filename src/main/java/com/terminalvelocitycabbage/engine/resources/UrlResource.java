d91793211bfd043a788cb01d3e289cf958d4f1b1
Brandon Davis <neusfear@gmail.com>
2020-09-27 15:42:22 -0500

    add png stuff to resource

--- src/main/java/com/terminalvelocitycabbage/engine/resources/UrlResource.java
+++ src/main/java/com/terminalvelocitycabbage/engine/resources/UrlResource.java
@@ -5,6 +5,7 @@ import org.apache.commons.io.IOUtils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
@@ -41,6 +42,11 @@ public class UrlResource implements Resource {
 			e.printStackTrace();
 		}
 		return out;
+	}
+
+	@Override
+	public Optional<ByteBuffer> getByteBufferFromPNG() {
+		return Optional.empty();
 	}
 
 	public boolean isInvalid() {
