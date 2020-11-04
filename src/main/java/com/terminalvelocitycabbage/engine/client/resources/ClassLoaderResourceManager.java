6b2ee4f18e6714cf541ba581d27974879c82a138
Brandon Davis <neusfear@gmail.com>
2020-11-04 17:18:03 -0600

    Allow shader root path specification

--- src/main/java/com/terminalvelocitycabbage/engine/client/resources/ClassLoaderResourceManager.java
+++ src/main/java/com/terminalvelocitycabbage/engine/client/resources/ClassLoaderResourceManager.java
@@ -8,15 +8,17 @@ public class ClassLoaderResourceManager implements ResourceManager {
 	private final List<UrlResource> loadedResources = new ArrayList<>();
 	private final ClassLoader classLoader;
 	private final String prefix;
+	private final String root;
 
-	public ClassLoaderResourceManager(ClassLoader classLoader, String prefix) {
+	public ClassLoaderResourceManager(ClassLoader classLoader, String prefix, String root) {
 		this.classLoader = classLoader;
 		this.prefix = prefix;
+		this.root = root;
 	}
 
 	@Override
 	public Optional<Resource> getResource(Identifier identifier) {
-		return Optional.ofNullable(classLoader.getResource(prefix + identifier.getNamespace() + "/" + identifier.getPath()))
+		return Optional.ofNullable(classLoader.getResource(prefix + "/" + identifier.getNamespace() + "/" + root + "/" + identifier.getPath()))
 				.map(url -> {
 					UrlResource resource = new UrlResource(identifier, url);
 					loadedResources.add(resource);
