d837105f1d0b08d5b5f5fcc7afc597d57c9c7844
Brandon Davis <neusfear@gmail.com>
2020-12-19 01:44:19 -0600

    Store animations on AnimatedModel

--- src/main/java/com/terminalvelocitycabbage/engine/client/renderer/model/AnimatedModel.java
+++ src/main/java/com/terminalvelocitycabbage/engine/client/renderer/model/AnimatedModel.java
@@ -1,20 +1,30 @@
 package com.terminalvelocitycabbage.engine.client.renderer.model;
 
 import com.terminalvelocitycabbage.engine.client.renderer.model.loader.AnimatedModelLoader;
+import com.terminalvelocitycabbage.engine.client.resources.Identifier;
+import com.terminalvelocitycabbage.engine.client.resources.ResourceManager;
+import net.dumbcode.studio.animation.info.AnimationInfo;
+import net.dumbcode.studio.animation.info.AnimationLoader;
 import net.dumbcode.studio.animation.instance.ModelAnimationHandler;
 import net.dumbcode.studio.model.ModelInfo;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class AnimatedModel extends Model {
 
+    public Map<String, AnimationInfo> animations;
     public final ModelAnimationHandler handler;
 
     public AnimatedModel(ModelInfo model) {
         super(model.getRoots().stream().map(AnimatedModelLoader.Part::createPart).collect(Collectors.toList()));
+
+        this.animations = new HashMap<>();
 
         List<AnimatedModelLoader.Part> partMap = new ArrayList<>();
         recursiveOnPart(modelParts, partMap::add);
@@ -25,6 +35,24 @@ public class AnimatedModel extends Model {
         for (Model.Part part : modelParts) {
             consumer.accept((AnimatedModelLoader.Part) part);
             recursiveOnPart(part.children, consumer);
+        }
+    }
+
+    public AnimationInfo addAnimation(String name, ResourceManager resourceManager, Identifier identifier) {
+        try {
+            AnimationInfo info = AnimationLoader.loadAnimation(resourceManager.getResource(identifier).orElseThrow().openStream());
+            animations.put(name, info);
+            return info;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public AnimationInfo getAnimation(String name) {
+        if (animations.containsKey(name)) {
+            return animations.get(name);
+        } else {
+            throw new RuntimeException("Could not get animation " + name + " in model " + this.toString() + " no value found.");
         }
     }
 }
