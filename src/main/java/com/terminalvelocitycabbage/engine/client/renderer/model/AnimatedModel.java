d401e6b95f8ea773215109d90eb118473924af23
Brandon Davis <neusfear@gmail.com>
2020-12-18 22:57:02 -0600

    start submodule swap

---
+++ src/main/java/com/terminalvelocitycabbage/engine/client/renderer/model/AnimatedModel.java
@@ -0,0 +1,14 @@
+package com.terminalvelocitycabbage.engine.client.renderer.model;
+
+import java.util.List;
+
+public class AnimatedModel extends Model {
+
+	//public final ModelAnimationHandler handler;
+
+	public AnimatedModel(List<Part> modelParts) {
+		super(modelParts);
+	}
+
+
+}
