e1ae197c4297f338359bda0b479940f7e832ae58
Brandon Davis <neusfear@gmail.com>
2021-01-31 12:37:57 -0600

    Simplify Camera base a bit

--- src/main/java/com/terminalvelocitycabbage/engine/client/renderer/components/Camera.java
+++ src/main/java/com/terminalvelocitycabbage/engine/client/renderer/components/Camera.java
@@ -9,22 +9,21 @@ public abstract class Camera {
 	private float clippingPlane;
 	private float farPlane;
 
-	private Matrix4f projectionMatrix;
+	private Matrix4f projectionMatrix = new Matrix4f();
 
 	public Camera(int fov, float clippingPlane, float farPlane) {
 		this.fov = (float)Math.toRadians(fov);
 		this.clippingPlane = clippingPlane;
 		this.farPlane = farPlane;
-
-		projectionMatrix = createProjectionMatrix(Renderer.getWindow().aspectRatio());
+		createProjectionMatrix(Renderer.getWindow().aspectRatio());
 	}
 
-	private Matrix4f createProjectionMatrix(float aspectRatio) {
-		return new Matrix4f().perspective(fov, aspectRatio, clippingPlane, farPlane);
+	private void createProjectionMatrix(float aspectRatio) {
+		projectionMatrix.identity().perspective(fov, aspectRatio, clippingPlane, farPlane);
 	}
 
 	public void updateProjectionMatrix(float aspectRatio) {
-		this.projectionMatrix = projectionMatrix.setPerspective(fov, aspectRatio, clippingPlane, farPlane);
+		this.projectionMatrix.setPerspective(fov, aspectRatio, clippingPlane, farPlane);
 	}
 
 	public Matrix4f getProjectionMatrix() {
