d9741f94c8f76a013cb4a70e5f5070305b4a959f
Brandon Davis <neusfear@gmail.com>
2021-02-21 14:29:09 -0600

    use the correct console for logging (oops)

--- src/main/java/com/terminalvelocitycabbage/engine/debug/Log.java
+++ src/main/java/com/terminalvelocitycabbage/engine/debug/Log.java
@@ -1,35 +1,37 @@
 package com.terminalvelocitycabbage.engine.debug;
 
+import org.fusesource.jansi.AnsiConsole;
+
 import static org.fusesource.jansi.Ansi.Color.*;
 import static org.fusesource.jansi.Ansi.ansi;
 
 public class Log {
 
 	public static void error(Object message) {
-		System.out.println( ansi().fg(RED).a("ERROR: ").reset().a(String.valueOf(message)).reset() );
+		AnsiConsole.out.println( ansi().fg(RED).a("[ERROR]: ").reset().a(String.valueOf(message)).reset() );
 	}
 
 	public static void error(Object message, String additionalInfo) {
-		System.out.println( ansi().fg(RED).a("ERROR: ").reset().a(String.valueOf(message)).reset() );
-		System.out.println( ansi().fg(RED).a(additionalInfo).reset() );
+		AnsiConsole.out.println( ansi().fg(RED).a("[ERROR]: ").reset().a(String.valueOf(message)).reset() );
+		AnsiConsole.out.println( ansi().fg(RED).a(additionalInfo).reset() );
 	}
 
 	public static void warn(Object message) {
-		System.out.println( ansi().fg(YELLOW).a("WARNING: ").reset().a(String.valueOf(message)).reset() );
+		AnsiConsole.out.println( ansi().fg(YELLOW).a("[WARNING]: ").reset().a(String.valueOf(message)).reset() );
 	}
 
 	public static void warn(Object message, String additionalInfo) {
-		System.out.println( ansi().fg(YELLOW).a("WARNING: ").reset().a(String.valueOf(message)).reset() );
-		System.out.println( ansi().fg(YELLOW).a(additionalInfo).reset() );
+		AnsiConsole.out.println( ansi().fg(YELLOW).a("[WARNING]: ").reset().a(String.valueOf(message)).reset() );
+		AnsiConsole.out.println( ansi().fg(YELLOW).a(additionalInfo).reset() );
 	}
 
 	public static void info(Object message) {
-		System.out.println( ansi().fg(BLUE).a("INFO: ").reset().a(String.valueOf(message)).reset() );
+		AnsiConsole.out.println(ansi().fg(BLUE).a("[INFO]: ").reset().a(String.valueOf(message)).reset());
 	}
 
 	public static void info(Object message, String additionalInfo) {
-		System.out.println( ansi().fg(BLUE).a("INFO: ").reset().a(String.valueOf(message)).reset() );
-		System.out.println( ansi().fg(BLUE).a(additionalInfo).reset() );
+		AnsiConsole.out.println( ansi().fg(BLUE).a("[INFO]: ").reset().a(String.valueOf(message)).reset() );
+		AnsiConsole.out.println( ansi().fg(BLUE).a(additionalInfo).reset() );
 	}
 
 }
