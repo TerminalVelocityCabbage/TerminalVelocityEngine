1f9edebbfce6b19fd48c994d1a329736ca6d9570
Brandon Davis <neusfear@gmail.com>
2021-08-15 15:35:36 -0500

    add vector util and docs

--- src/main/java/com/terminalvelocitycabbage/engine/utils/VectorUtils.java
+++ src/main/java/com/terminalvelocitycabbage/engine/utils/VectorUtils.java
@@ -4,8 +4,23 @@ import org.joml.Vector3f;
 
 public class VectorUtils {
 
+    /**
+     * @param v1 the first vector of the line
+     * @param v2 the last vector of the line
+     * @return a new Vector3f at the middle of the line drawn between v1 and v2
+     */
     public static Vector3f find2Vec3Mid(final Vector3f v1, final Vector3f v2) {
         return new Vector3f((v1.x + v2.x) / 2f, (v1.y + v2.y) / 2f, (v1.z + v2.z) / 2f);
+    }
+
+    /**
+     * @param startVec The start of the line
+     * @param endVec The end of the line
+     * @param position The percentage (0-1) the resultant vector should be placed along the line between start and end
+     * @return a new Vector3f at the requested location
+     */
+    public static Vector3f findPercentDistVector(final Vector3f startVec, final Vector3f endVec, float position) {
+        return new Vector3f((startVec.x + endVec.x) * position, (startVec.y + endVec.y) * position, (startVec.z + endVec.z) * position);
     }
 
 }
